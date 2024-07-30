c5f24c2f056c22f6a2c4fc2fb4b915fa10b75ab0
CengizhanOemuer <cengiz.cmr@gmail.com>
2024-07-30 20:25:23 +0200

    feat: added basic program arguments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,34 @@
+use std::env;
+
 fn main() {
     println!("Hello, world!");
     
+    let args: Vec<String> = env::args().collect();
+
+    dbg!(args);
+}
+
+fn parse_arguments(args: Vec<&str>) {
+    let command = args[1];
+
+    match command {
+        "add" => {
+            
+        }, 
+        "update" => {
+
+        },
+        "delete" => {
+
+        },
+        "show" => {
+
+        },
+        "help" => {
+
+        },
+        &_ => {
+
+        }
+    }
 }
