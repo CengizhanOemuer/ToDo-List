b6b4cdb6f2784bcb14b6e5a8301c15c7878609a3
CengizhanOemuer <cengiz.cmr@gmail.com>
2024-07-31 17:28:15 +0200

    refactor: deleted most of the code and changed some crates!

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,25 @@
-use std::env;
+use std::fs::{self, File};
+use std::io::{self, Write, BufRead, BufReader};
+use std::path::{Path, PathBuf};
+use dirs::home_dir;
 
 fn main() {
-    println!("Hello, world!");
-    
-    let args: Vec<String> = env::args().collect();
+    // Initalize application:
+    create_application_folder();
 
-    dbg!(args);
 }
 
-fn parse_arguments(args: Vec<&str>) {
-    let command = args[1];
+fn create_application_folder() {
+    // Get home_dir:
+    let mut dir = home_dir().expect("Could not find home directory!");
 
-    match command {
-        "add" => {
-            
-        }, 
-        "update" => {
+    // Append path to application dir:
+    dir.push(".to-do-lists");
 
-        },
-        "delete" => {
-
-        },
-        "show" => {
-
-        },
-        "help" => {
-
-        },
-        &_ => {
-
-        }
+    // Check wheter directory does not exists:
+    if !dir.exists() {
+        // Create directory:
+        fs::create_dir(&dir).expect("Could not create the application directory!");
+        println!("Successfully created application directory!")
     }
 }
