87a2229758f603c3b6fdb890c30e8f78698f9200
CengizhanOemuer <cengiz.cmr@gmail.com>
2024-08-15 22:34:19 +0200

    feat: refactored add_to_do(), open_to_do_list(), run_application and also added update_to_do()

--- src/main.rs
+++ src/main.rs
@@ -18,28 +18,51 @@ fn main() {
 }
 
 fn run_application() {
-    println!("To-Do-List-Application\n");
-    println!("Choose what u want to do:");
-    println!("1 --> Create a Todo-List");
-    println!("2 --> Show already existing Todo-Lists");
-    println!("3 --> Open a Todo-List");
-    println!("4 --> Delete a Todo-List");
-    println!("Wrong input will result in exiting the application!\n");
+    loop {
+        println!("To-Do-List-Application\n");
+        println!("Choose what u want to do:");
+        println!("1 --> Create a Todo-List");
+        println!("2 --> Open a Todo-List");
+        println!("3 --> Delete a Todo-List");
+        println!("4 --> Show already existing Todo-Lists");
+        println!("Wrong input will result in exiting the application!\n");
 
-    // Get user_input:
-    let mut user_input = String::new();
-    io::stdin().read_line(&mut user_input).expect("Failed to read line!");
+        // Get user_input:
+        let mut user_input = String::new();
+        io::stdin().read_line(&mut user_input).expect("Failed to read line!");
 
-    // Format user_input:
-    let user_input : i32 = user_input.trim().parse().expect("Not a valid number!");
+        // Format user_input:
+        let user_input : i32 = user_input.trim().parse().expect("Not a valid number!");
 
-    match user_input {
-        1 => {create_to_do_list();}
-        2 => {show_to_do_lists();}
-        3 => {open_to_do_list();}
-        4 => {delete_to_do_list();}
-        5 => {add_to_do(String::from("MeinTest"));}
-        _ => {exit(0);}
+        match user_input {
+            1 => {create_to_do_list();}
+            2 => {
+                let file = open_to_do_list().unwrap();
+                loop {
+                    println!("\n Choose between:\n1 --> Add a task\n2 --> Update a task\n3 --> Close Todo-List");
+
+                    // Get user_input:
+                    let mut user_input = String::new();
+                    io::stdin().read_line(&mut user_input).expect("Failed to read line!");
+
+                    // Format user_input:
+                    let user_input : i32 = user_input.trim().parse().expect("Not a valid number!");
+
+                    match user_input {
+                        1 => {add_to_do(&file);}
+                        2 => {update_to_do(&file);}
+                        3 => {
+                            println!("Closed Todo-List!");
+                            break;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            3 => {delete_to_do_list();}
+            4 => {show_to_do_lists();}
+            _ => {exit(0);}
+        }
     }
 }
 
@@ -96,7 +119,7 @@ fn show_to_do_lists() {
     }
 }
 
-fn open_to_do_list() {
+fn open_to_do_list() -> Option<PathBuf> {
     println!(); 
     let dir = get_application_folder();
 
@@ -116,8 +139,10 @@ fn open_to_do_list() {
         for line in reader.lines() {
             println!("{}", line.expect("Could not read line!"));
         }
+        Some(file_path)
     } else {
         println!("To-do list not found: {}", name);
+        None
     }
 }
 
@@ -143,13 +168,8 @@ fn delete_to_do_list() {
     }
 }
 
-fn add_to_do(filename: String) {
+fn add_to_do(file_path: &PathBuf) {
     let dir = get_application_folder();
-
-    // Build file_path:
-    let filename = filename.trim();
-    let mut file_path = dir.join(filename);
-    file_path.set_extension("txt");
 
     // Open file with append option:
     let mut file = OpenOptions::new()
@@ -168,22 +188,75 @@ fn add_to_do(filename: String) {
 
     // Create task:
     let task = Task {
-        name: name,
-        description: description,
+        name: name.trim().to_string(),
+        description: description.trim().to_string(),
         finished: false
     };
 
-    file.write(task.name.as_bytes()).expect("Write failed!");
-    file.write(task.description.as_bytes()).expect("Write failed!");
-    file.write(task.finished.to_string().as_bytes()).expect("Write failed!");
+     // Format task as XML:
+     let xml_data = format!(
+        "<task>\n   <name>{}</name>\n   <description>{}</description>\n   <finished>{}</finished>\n</task>\n",
+        task.name,
+        task.description,
+        task.finished
+    );
+
+    file.write_all(xml_data.as_bytes()).expect("Could not write in file!");
 
     println!("Appended task to file!");
-
-
 }
 
-fn update_to_do() {
+fn update_to_do(file_path: &PathBuf) {
+    // Open file:
+    let file = File::open(file_path).expect("Could not open file!");
+    let reader = BufReader::new(file);
 
+    // Read lines into a vector:
+    let mut lines: Vec<String> = reader.lines().map(|l| l.expect("Could not read line")).collect();
+
+    // Get the name of the task to update:
+    println!("Enter the name of the task to mark it as finished:");
+    let mut task_name = String::new();
+    io::stdin().read_line(&mut task_name).expect("Failed to read line!");
+    let task_name = task_name.trim();
+
+    // Flag to indicate if the task was found and updated:
+    let mut task_found = false;
+
+    // Iterate through the lines and update the task's finished status:
+    for i in 0..lines.len() {
+        if lines[i].trim() == format!("<name>{}</name>", task_name) {
+            // Find the next line with <finished> and set it to true:
+            for j in i + 1..lines.len() {
+                if lines[j].trim().starts_with("<finished>") {
+                    lines[j] = "   <finished>true</finished>".to_string();
+                    task_found = true;
+                    break;
+                }
+            }
+        }
+        if task_found {
+            break;
+        }
+    }
+
+    // If the task was found and updated, write the lines back to the file
+    if task_found {
+        let mut file = OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .open(file_path)
+            .expect("Could not open file for writing!");
+
+        for line in lines {
+            file.write_all(line.as_bytes()).expect("Could not write to file");
+            file.write_all(b"\n").expect("Could not write newline");
+        }
+
+        println!("Task '{}' marked as finished!", task_name);
+    } else {
+        println!("Task '{}' not found!", task_name);
+    }
 }
 
 fn delete_to_do() {
